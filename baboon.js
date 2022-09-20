10597569d4695a7849490260f0f376350ebea60f
Alex Zen <108775988+azen12@users.noreply.github.com>
2022-09-20 14:10:14 -0400

    Update baboon.js
    some logging for values, more functionality will be added soon

--- baboon.js
+++ baboon.js
@@ -1,16 +1,29 @@
 let button = document.getElementById('testing');
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let feedback = document.getElementById('feedback');
+let forms = document.getElementById('forms');
+let numberInput = document.getElementById('numbers');
 
+button.addEventListener('click', unblocker);
+forms.addEventListener('submit', onSubmit);
 
-
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(fname.value);
+    console.log(lname.value);
+    console.log(feedback.value);
+    random();
+};
 
 function unblocker() {
     document.getElementById('closed').style.display = 'block';
 };
 
+function random() {
+    let randoms = Math.floor(Math.random() * (numberInput.value) + 1);
+    console.log(randoms);
+};
 
-button.addEventListener('click', unblocker);
 
 
-function random(number) {
-    return Math.floor(Math.random() * (number+1));
-  };
